da2453fa80c0f89b0dec51968150d713dfef760c
mzeleny83 <110114227+mzeleny83@users.noreply.github.com>
2024-06-16 08:58:39 +0200

    Add files via upload

---
+++ Hello world_colored.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < helloWorld.lengt; i++) {
+    const span=document.createElement("span");
+    span.innerText("HelloWorld[i]");
+    span.styleColor=´rgb(
+        $(Math.floor(Math.random())*255)},
+        $(Math.floor(Math.random())*255)},
+        $(Math.floor(Math.random())*255)}´;
+
+        span.style.fontSize=´${Math.floor(math.random*100)+70}px´;
+        document.body.appendChild(span);
+        
+    
